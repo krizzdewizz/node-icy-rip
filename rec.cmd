2317e8b10eb9559e9e5f184df6fa593b85db3b4c
krizzdewizz <gogo-900>
2015-03-11 03:24:46 +0100

    move to test

--- rec.cmd
+++ rec.cmd
@@ -1,2 +1,2 @@
-rem node index http://85.25.86.69:8000
-node index http://yp.shoutcast.com/sbin/tunein-station.pls?id=528480
+rem node scripts/test/rec http://85.25.86.69:8000
+node scripts/test/rec http://yp.shoutcast.com/sbin/tunein-station.pls?id=528480
---
+++ scripts/test/rec.ts
@@ -0,0 +1,11 @@
+﻿import path = require('path');
+import icy = require('../icy');
+
+icy.main({
+    //url: 'http://85.25.86.69:8000', // icecast stream
+    url: 'http://173.193.14.170:9008', // airlesss
+    //    url: 'http://yp.shoutcast.com/sbin/tunein-station.pls?id=709809', // with redirects
+    //url: 'http://yp.shoutcast.com/sbin/tunein-station.m3u?id=175821', // m3u
+    outputFolder: path.join(process.cwd(), 'recordings'),
+    teeToStdout: false
+});
